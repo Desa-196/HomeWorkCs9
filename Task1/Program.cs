16e4bd473a3970138a9fb71a1a8231bcd53dda6b
Денис Пономарев <deskin196@gmail.com>
2023-04-03 11:29:51 +0300

    Создал проект по домашнему заданию 1

---
+++ Task1/Program.cs
@@ -0,0 +1,28 @@
+﻿/*
+Задача 64: Задайте значение N. Напишите программу, которая выведет все натуральные числа в промежутке от N до 1. Выполнить с помощью рекурсии.
+
+N = 5 -> "5, 4, 3, 2, 1"
+N = 8 -> "8, 7, 6, 5, 4, 3, 2, 1"
+
+*/
+
+int GetIntFromConsole()
+{
+    int console_int = 0;
+
+    Console.Write("Введите кол-во строк: ");
+
+    while(true)
+    {
+        if( int.TryParse(Console.ReadLine(), out console_int ) )
+        {
+            break;
+        }
+        else
+        {
+            Console.Write("Введено некорректное число, повторите попытку ввода:");
+        }
+    }
+    return console_int;
+
+}
