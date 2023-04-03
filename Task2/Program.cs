9efc3513ae51e6aa542934261f46e28313fdb8aa
Денис Пономарев <deskin196@gmail.com>
2023-04-03 12:03:54 +0300

    Написал алгоритм по ДЗ 2

--- Task2/Program.cs
+++ Task2/Program.cs
@@ -5,3 +5,35 @@ M = 1; N = 15 -> 120
 M = 4; N = 8. -> 30
 */
 
+int ConsoleReadInt(string text)
+{
+    Console.Write($"{text}: ");
+
+    int read_int;
+
+    while (true)
+    {
+
+
+        if (int.TryParse(Console.ReadLine(), out read_int))
+        {
+            return read_int;
+        }
+        else
+        {
+            Console.Write("Введено некорректное число, повторите попытку ввода:");
+        }
+
+    }
+}
+
+int Sum(int m, int n)
+{
+    if (m == n) return n;
+    return m + Sum(m + 1, n);
+}
+
+int m = ConsoleReadInt("Введите значение М");
+int n = ConsoleReadInt("Введите значение N");
+
+Console.WriteLine(  $"сумму натуральных элементов в промежутке от M до N равна {Sum(m, n)}"    );
