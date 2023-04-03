7d053d788baaad98feb679f6b313b28719ce442d
Денис Пономарев <deskin196@gmail.com>
2023-04-03 12:34:25 +0300

    Написал алгоритм по ДЗ 3

--- Task3/Program.cs
+++ Task3/Program.cs
@@ -4,3 +4,38 @@ m = 2, n = 3 -> A(m,n) = 9
 m = 3, n = 2 -> A(m,n) = 29
 
 */
+
+int ConsoleReadInt(string text)
+{
+    Console.Write($"{text}: ");
+
+    int read_int;
+
+    while (true)
+    {
+
+
+        if (int.TryParse(Console.ReadLine(), out read_int))
+        {
+            return read_int;
+        }
+        else
+        {
+            Console.Write("Введено некорректное число, повторите попытку ввода:");
+        }
+
+    }
+}
+
+int Accerman(int m, int n)
+{
+    if (m == 0) return n + 1;
+    else if (n == 0) return Accerman(m - 1, 1);
+    else return Accerman(m - 1, Accerman(m, n - 1));
+}
+
+
+int m = Math.Abs(ConsoleReadInt("Введите m"));
+int n = Math.Abs(ConsoleReadInt("Введите n"));
+
+Console.WriteLine(Accerman(m, n));
